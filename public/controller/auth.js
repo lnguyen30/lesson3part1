8555e875fe83779a4d420e48243c56952559b3b8
Long Nguyen <lnguyen30@uco.edu>
2021-05-23 17:24:01 -0500

    state change when user signs in or out

--- public/controller/auth.js
+++ public/controller/auth.js
@@ -2,6 +2,7 @@ import * as Element from '../viewpage/element.js'
 import * as FirebaseController from './firebase_controller.js'
 import * as Constant from '../model/constant.js'
 import * as Util from '../viewpage/util.js'
+import * as Route from './route.js'
 
 export let currentUser;
 
@@ -45,6 +46,9 @@ export function addEventListeners(){
         for(let i=0; i<elements.length; i++){
             elements[i].style.display ='block';
         }
+
+        Route.routing(window.location.pathname, window.location.hash);
+        
     }else{
         currentUser = user;
         let elements = document.getElementsByClassName('modal-pre-auth');
@@ -55,7 +59,8 @@ export function addEventListeners(){
         for(let i=0; i<elements.length; i++){
             elements[i].style.display ='none';
         }
-
+        history.pushState(null, null, Route.routePathnames.HOME);
+        Route.routing(window.location.pathname, window.location.hash);
     }
   });
 
