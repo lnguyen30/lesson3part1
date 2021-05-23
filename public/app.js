be3dbc3bd7ac01488ae42d47b597a7e5076c7e44
Long Nguyen <lnguyen30@uco.edu>
2021-05-23 01:22:53 -0500

    in progress route path function

--- public/app.js
+++ public/app.js
@@ -3,10 +3,17 @@ import * as Home from './viewpage/home_page.js'
 import * as Purchase from './viewpage/purchase_page.js'
 import * as Cart from './viewpage/cart.js'
 import * as Profile from './viewpage/profile_page.js'
-
+import * as Route from './controller/route.js'
 
 Auth.addEventListeners();
 Home.addEventListeners();
 Purchase.addEventListeners();
 Cart.addEventListeners();
 Profile.addEventListeners();
+
+window.onload = ()=>{
+    //fetches the url of the page then passes it to the routing function
+    const pathname = window.location.pathname;
+    const hash = window.location.hash;
+    Route.routing(pathname, hash)
+}
---
+++ public/controller/route.js
@@ -0,0 +1,35 @@
+import * as Home from '../viewpage/home_page.js'
+import * as Purchase from '../viewpage/purchase_page.js'
+import * as Cart from '../viewpage/cart.js'
+import * as Profile from '../viewpage/profile_page.js'
+
+export const routePathnames ={
+    HOME: '/',
+    PURCHASE: '/purchase',
+    PROFILE: '/profile',
+    CART: '/cart',
+}
+
+// routes for url when buttons are clicked and the functions that associate with the route
+export const routes = [
+    {pathname: routePathnames.HOME, page: Home.home_page}, 
+    {pathname: routePathnames.PURCHASE, page: Purchase.purchase_page}, 
+    {pathname: routePathnames.CART, page: Cart.cart_page}, 
+    {pathname: routePathnames.PROFILE, page: Profile.profile_page}, 
+];
+
+export function routing(pathname, hash){
+    //checks to see if path exists
+    const route = routes.find(r=> r.pathname == pathname);
+    //if route exists find the page function associated to path
+    if(route) route.page();
+    else routes[0].page(); //default page
+}
+
+
+window.addEventListener('popstate', e =>{ // updates url after user presses forward or backward 
+    e.preventDefault();
+    const pathname = e.target.location.pathname;
+    const hash = e.target.location.hash;
+    Route.routing(pathname, hash);
+});
