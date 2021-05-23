b3c8afb09391f7cc54483a25842e4ef7f544070e
Long Nguyen <lnguyen30@uco.edu>
2021-05-22 22:26:24 -0500

    cleaned up auth file

--- public/controller/auth.js
+++ public/controller/auth.js
@@ -1,10 +1,7 @@
 import * as Element from '../viewpage/element.js'
 import * as FirebaseController from './firebase_controller.js'
-<<<<<<< HEAD
 import * as Constant from '../model/constant.js'
-=======
 import * as Util from '../viewpage/util.js'
->>>>>>> les31-13-modal-infobox
 //event listener for sign in
 export function addEventListeners(){
     Element.formSignin.addEventListener('submit', async e => {
@@ -18,13 +15,8 @@ export function addEventListeners(){
             await FirebaseController.signIn(email, password);
             Element.modalSignin.hide();
         }catch(e){
-<<<<<<< HEAD
-          console.log(e);
-=======
-            console.log(e);
+           if (Constant.DEV) console.log(e);
             Util.info('Sign In Error', JSON.stringify(e), Element.modalSignin);
-            
->>>>>>> les31-13-modal-infobox
         }
     })
 
