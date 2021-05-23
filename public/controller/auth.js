8a7453b96573d2bfcb3720c7bc1faab93425d423
Long Nguyen <lnguyen30@uco.edu>
2021-05-22 20:43:19 -0500

    signin to firebase

---
+++ public/controller/auth.js
@@ -0,0 +1,20 @@
+import * as Element from '../viewpage/element.js'
+import * as FirebaseController from './firebase_controller.js'
+//event listener for sign in
+export function addEventListeners(){
+    Element.formSignin.addEventListener('submit', async e => {
+
+         //listens to sign in form when sign in form button is clicked
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        try{
+            await FirebaseController.signIn(email, password);
+            Element.modalSignin.hide();
+        }catch(e){
+            console.log(e);
+        }
+    })
+
+} 
--- public/viewpage/element.js
+++ public/viewpage/element.js
@@ -5,3 +5,10 @@ export const menuPurchases = document.getElementById('menu-purchases');
 export const menuSignOut = document.getElementById('menu-signout');
 export const menuCart = document.getElementById('menu-cart');
 export const menuProfile = document.getElementById('menu-profile');
+
+
+//forms
+export const formSignin = document.getElementById('form-signin');
+
+//modal
+export  const modalSignin = new bootstrap.Modal(document.getElementById('modal-signin'), {backdrop: 'static'});
